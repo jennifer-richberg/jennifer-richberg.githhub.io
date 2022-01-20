8a4f6541d58c7d76ce6cb8b80c2b8fdf8a08a844
jennifer-richberg <65781366+jennifer-richberg@users.noreply.github.com>
2022-01-20 13:38:57 -0600

    Add files via upload

---
+++ Text-Base Adventure Game.py
@@ -0,0 +1,71 @@
+###Enter you own information in the text-base Adventure Game
+
+def scene1():
+    import time
+    print(#"""Enter in story""")
+    
+    c1 = input()
+    time.sleep(2)
+    ans = 'incorrect'
+    while(ans=='incorrect')
+        if(c1.upper()==(#"Enter Choice 1"))
+            print(#"Continue story")
+            ans = 'correct'
+        elif(c1.upper()==(#"Enter Choice 2"))
+            print(#"Continue story")
+            ans = 'correct'
+            scene2()
+        else:
+            print("Enter the correct choice!")
+            c1 = input
+
+def scene2():
+    import time
+    print(#"""Enter in story""")
+    
+    c2 = input()
+    time.sleep(2)
+    ans = 'incorrect'
+    while(ans=='incorrect')
+        if(c2.upper()==(#"Enter Choice 1"))
+            print(#"Continue story")
+            ans = 'correct'
+        elif(c2.upper()==(#"Enter Choice 2"))
+            print(#"Continue story")
+            ans = 'correct'
+            scene2()
+        else:
+            print("Enter the correct choice!")
+            c2 = input
+            
+def scene3():
+    import time
+    print(#"""Enter in story""")
+    
+    c3 = input()
+    time.sleep(2)
+    ans = 'incorrect'
+    while(ans=='incorrect')
+        if(c3.upper()==(#"Enter Choice 1"))
+            print(#"Continue story")
+            ans = 'correct'
+        elif(c3.upper()==(#"Enter Choice 2"))
+            print(#"Continue story")
+            ans = 'correct'
+            scene2()
+        else:
+            print("Enter the correct choice!")
+            c3 = input
+    time.sleep(2)
+    scene3(pick)
+            
+def scene4(pick_value):
+    import time
+    print(#Enter story)
+       if(pick_value=='True'):
+           time.sleep(2)
+           print(#Enter story)
+           time.sleep(2)
+           print(#Enter story)
+      elif(pick_value=='False')
+          print(#Enter story)
