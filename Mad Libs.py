e4cb22c6b13a70686380488444f53a5daecba76c
jennifer-richberg <65781366+jennifer-richberg@users.noreply.github.com>
2022-01-20 07:47:07 -0600

    Add files via upload

---
+++ Mad Libs.py
@@ -0,0 +1,28 @@
+# # This Project is Mad Libs
+
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+type1 = input("Enter a type of bird: ")
+type2 = input("Enter a room in a house: ")
+verb1 = input("Verb(past tense): ")
+verb2 = input("Verb: ")
+noun1 = input("Relative's name: ")
+noun2 = input("Enter a noun: ")
+type3 = input("Enter a liquid: ")
+verb3 = input("Enter a verb ending in-ing: ")
+type4 = input("Enter part of the body(plural): ")
+noun3 = input("Enter a plural noun: ")
+verb4 = input("Enter a verb ending in-ing: ")
+noun4 = input("Enter a noun: ") 
+
+
+madlib = f"It was a {adj1}, cold November day. I woke up to the {adj2} smell of \
+{type1} roasting in the {type2} downstairs. I {verb1} down the stairs to see if I could \
+help {verb2} the dinner. My mom said, See if {noun1} needs a fresh {noun2}. So I carried \
+a tray of glasses full of {type3} into the {verb3} room. When I got there, I couldn't \
+believe my {type4}! There were {noun3} {verb4} on the {noun4}!"
+
+print(madlib)
+
+
+
